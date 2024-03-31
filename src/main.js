678b69a350774ebd0b4cd2b39dd6e89b9609d858
Thena Seer <11550396+blueridger@users.noreply.github.com>
2024-03-30 23:02:40 -0700

    1.0.4 bugfix using onChanged

--- src/main.js
+++ src/main.js
@@ -1,29 +1,34 @@
 let pattern = "https://www.dndbeyond.com/*";
-
-function buildRedirect(savedParams) {
-  return function redirect(requestDetails) {
-    console.log(`Redirecting: ${requestDetails.url}`);
-    url = new URL(requestDetails.url)
-    params = new URLSearchParams(url.search);
-    if (savedParams && !params.has("filter-source")) {
-      for (const value of savedParams)
-        params.append("filter-source", value)
-      url.search = params.toString()
-      return {
-        redirectUrl: url.toString(),
-      };
-    } else {
-      browser.storage.sync.set({"currentParams": params.getAll("filter-source")});
-    }
-  };
+let savedParams = null;
+function redirect(requestDetails) {
+  console.log(`Redirecting: ${requestDetails.url}`);
+  console.log("s:"+savedParams)
+  url = new URL(requestDetails.url);
+  params = new URLSearchParams(url.search);
+  if (savedParams && !params.has("filter-source")) {
+    for (const value of savedParams) params.append("filter-source", value);
+    url.search = params.toString();
+    return {
+      redirectUrl: url.toString(),
+    };
+  } else {
+    browser.storage.sync.set({ currentParams: params.getAll("filter-source") });
+  }
 }
-browser.storage.sync.remove(["currentParams"])
-browser.storage.sync
-  .get(["savedParams"])
-  .then((result) =>
-    browser.webRequest.onBeforeRequest.addListener(
-      buildRedirect(result.savedParams),
-      { urls: [pattern], types: ["main_frame"] },
-      ["blocking"]
-    )
+
+browser.storage.sync.remove(["currentParams"]);
+browser.storage.sync.get(["savedParams"]).then((result) => {
+  savedParams = result.savedParams;
+  browser.webRequest.onBeforeRequest.addListener(
+    redirect,
+    { urls: [pattern], types: ["main_frame"] },
+    ["blocking"]
   );
+});
+browser.storage.sync.onChanged.addListener((changes) => {
+  console.log(changes)
+  const changedItems = Object.keys(changes);
+  for (const item of changedItems) {
+    if (item == "savedParams") savedParams = changes[item].newValue;
+  }
+});
